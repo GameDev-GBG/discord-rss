0dd6115c7d3b66b029cd50a1dfdacc63ecc823f3
Sebastian Strand <sebastian@yukigunigames.com>
2026-02-03 14:57:10 +0100

    Add Discord webhook posting + related stuff

---
+++ envdefault.py
@@ -0,0 +1,19 @@
+# Source - https://stackoverflow.com/a/10551190
+# Posted by Russell Heilling, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-03, License - CC BY-SA 4.0
+
+import argparse
+import os
+
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, 
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import time
 import aiohttp
+import aiofiles
 import asyncio
 import feedparser
+from feedparser import FeedParserDict
+import argparse
 from rich.console import Console
 from rich.progress import Progress
 from rich.table import Table
+from discord_webhook import AsyncDiscordWebhook
+from envdefault import EnvDefault
+from dotenv import load_dotenv
+
+load_dotenv()
 
 console = Console()
 
-async def fetch_rss(session, url, progress, task_progress):
+async def fetch_rss(session : aiohttp.ClientSession, url, progress, task_progress):
     # console.log(f"Fetching feed: {url}")
     progress.update(task_progress, description = f"{url} Fetching...")
     try:
@@ -23,15 +31,15 @@ async def fetch_rss(session, url, progress, task_progress):
         progress.update(f"{url} Failed to fetch ❌")
     return(url, None)
 
-async def main():
-    urls = [
-        "https://bsky.app/profile/yukiguni.games/rss",
-        "https://bsky.app/profile/moonhood.bsky.social/rss",
-        "https://bsky.app/profile/riverendgames.bsky.social/rss",
-        "https://bsky.app/profile/splendidfailures.bsky.social/rss",
-        "https://bsky.app/profile/ycjygames.bsky.social/rss",
-        "https://mastodon.gamedev.place/@yukigunigames.rss"
-    ]
+async def main(filename : str, webhook_url : str):
+    urls = []
+    async with aiofiles.open(filename, mode='r') as f:
+        async for line in f:
+            urls.append(line)
+
+    if urls.count == 0:
+        console.log(f"\"{filename}\" has no entries")
+        return
 
     with Progress(console=console) as progress:
         url_progresses = []
@@ -50,6 +58,7 @@ async def main():
         table.add_column("Description")
         table.add_column("Published")
 
+        webhooks = []
         for index, (url, text) in enumerate(results):
             if text == None:
                 continue
@@ -63,12 +72,31 @@ async def main():
             if index != 0:
                 table.add_section()
 
-            for e in feed.entries:
+            for index, e in enumerate(feed.entries):
                 table.add_row(e.link, e.description, e.published)
+                if index == 0:
+                    webhooks.append(send_webhook(webhook_url, e))
         
         console.print(table)
+        await asyncio.gather(*webhooks)
+
+async def send_webhook(webhook_url : str, entry : FeedParserDict):
+    webhook = AsyncDiscordWebhook(webhook_url, content = entry.link)
+    await webhook.execute()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog = 'Discord RSS Socials',
+        description= 'Polls RSS feeds and posts updates to Discord via a webhook.',
+        epilog= 'Good bye!'
+    )
+    parser.add_argument('filename', type=str,
+                        help="Path to a file containing URL:s to RSS feeds, separated by new lines.")
+    parser.add_argument('-wh', '--webhook', action=EnvDefault, type=str, envvar='DISCORD_WEBHOOK',
+                        help='A Discord webhook URL. You can generate this for a channel under Edit Channel/Integrations/Webhooks.\n' \
+                            'Can also be specified using DISCORD_WEBHOOK environment variable.')
+    
+    args = parser.parse_args()
     start_time = time.time()
-    asyncio.run(main())
+    asyncio.run(main(args.filename, args.webhook))
     console.log(f"Total time taken: {time.time() - start_time} seconds")
