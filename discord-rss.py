d08c72364764b254a75a38ae891d91d0d6e0826f
Sebastian Strand <sebastian@yukigunigames.com>
2026-02-05 14:55:15 +0100

    Add better logging and more graceful exception handling

--- discord-rss.py
+++ discord-rss.py
@@ -3,9 +3,9 @@ import aiofiles
 import asyncio
 import feedparser
 import argparse
-from rich.console import Console
 from rich.table import Table
 from rich.live import Live
+from rich.logging import RichHandler
 from discord_webhook import AsyncDiscordWebhook
 from envdefault import EnvDefault
 from dotenv import load_dotenv
@@ -14,8 +14,14 @@ from typing import Dict
 import datetime
 import http.client
 from dateutil import parser as dateparser
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+from urllib.parse import urlparse
 
-load_dotenv()
+load_dotenv(".env")
+
+logger = logging.getLogger(__name__)
 
 class RssEntry:
     def __init__(self, feed: RssFeed, url : str, time_posted : datetime.datetime):
@@ -24,7 +30,7 @@ class RssEntry:
         self.time_posted=time_posted
 
 class RssFeed:
-    def __init__(self, url : str, console: Console):
+    def __init__(self, url : str):
         self.url : str = url
         self.http_etag : str = None
         self.http_modified : str = None
@@ -36,7 +42,6 @@ class RssFeed:
         self.user_name : str = None
         self.avatar_url : str = None
         self.status : str = ":rocket:"
-        self.console = console
 
     async def check(self, since : datetime.datetime, queue : asyncio.Queue[RssEntry]):
         self.last_checked = datetime.datetime.now()
@@ -113,6 +118,15 @@ def date_time_from_struct(t : time.struct_time):
 
 # ---- Checking Feeds ----
 
+async def check_feeds_task(feeds : List[RssFeed], since : datetime.datetime, queue : asyncio.Queue[RssEntry]):
+    while True:
+        logger.info("Checking feeds...")
+        t = time.time()
+        await check_feeds(feeds, since, queue)
+        logger.info(f"Took {time.time() - t} seconds")
+
+        await asyncio.sleep(10)
+
 async def check_feeds(feeds : List[RssFeed], since : datetime.datetime, queue : asyncio.Queue[RssEntry]):
     checks = []
     for feed in feeds:
@@ -121,13 +135,13 @@ async def check_feeds(feeds : List[RssFeed], since : datetime.datetime, queue : 
 
 # ---- Posting ----
 
-async def run_webhook_queue(console : Console, webhook_url : str, queue : asyncio.Queue[RssEntry]):
+async def webhook_queue_task(webhook_url : str, queue : asyncio.Queue[RssEntry]):
     while True:
         rss_entry = await queue.get()
-        await send_webhook(console, webhook_url, rss_entry)
+        await send_webhook(webhook_url, rss_entry)
 
-async def send_webhook(console : Console, webhook_url : str, entry : RssEntry):
-    console.log(F"We are sending {entry.url} which was posted at {entry.time_posted}")
+async def send_webhook(webhook_url : str, entry : RssEntry):
+    logger.info(F"We are sending {entry.url} which was posted at {entry.time_posted}")
 
     webhook = AsyncDiscordWebhook(webhook_url, content = entry.url)
     webhook.username = entry.feed.user_name
@@ -135,9 +149,9 @@ async def send_webhook(console : Console, webhook_url : str, entry : RssEntry):
     webhook.rate_limit_retry = True
     resp = await webhook.execute()
     if resp.status_code == http.client.OK:
-        console.log(f"Sent successfully!")
+        logger.info(f"Sent successfully!")
     else:
-        console.log(f"Sending post failed: {resp.status_code} {http.client.responses[resp.status_code]}")
+        logger.info(f"Sending post failed: {resp.status_code} {http.client.responses[resp.status_code]}")
 
 # ---- Render ----
 def render_table(feeds : List[RssFeed], since : datetime.datetime) -> Table:
@@ -152,42 +166,21 @@ def render_table(feeds : List[RssFeed], since : datetime.datetime) -> Table:
         table.add_row(str(feed.title), str(feed.status), str(feed.last_checked), str(feed.updated_str), str(feed.http_etag), str(feed.http_modified))
     return table
 
-async def render(console : Console, feeds : List[RssFeed], since : datetime.datetime):
-    with Live(render_table(feeds, since), auto_refresh=False, console=console) as live:
+async def render(feeds : List[RssFeed], since : datetime.datetime):
+    with Live(render_table(feeds, since), auto_refresh=False) as live:
         while True:
             await asyncio.sleep(0.25)
             live.update(render_table(feeds, since), refresh=True)
 
-async def main(filename : str, webhook_url : str, since : datetime.datetime, should_render : bool):
-    console = Console()
-    feeds = []
-    async with aiofiles.open(filename, mode='r') as f:
-        async for line in f:
-            feeds.append(RssFeed(line, console))
+# Kudos: https://www.slingacademy.com/article/python-ways-to-check-if-a-string-is-a-valid-url/
+def is_valid_url(url : str):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
-    if feeds.count == 0:
-        console.log(f"\"{filename}\" has no entries")
-        return
-
-    queue = asyncio.Queue[RssEntry]()
-
-    # Spin up queue routine which pushes out new webhooks
-    asyncio.create_task(run_webhook_queue(console, webhook_url, queue))
-
-    # Spin up rendering routine if requested
-    if should_render == True:
-        asyncio.create_task(render(console, feeds, since))
-
-    # Just endlessly check feeds and push to the queue
-    while True:
-        console.log("Checking feeds...")
-        t = time.time()
-        await check_feeds(feeds, since, queue)
-        console.log(f"Took {time.time() - t} seconds")
-
-        await asyncio.sleep(10)
-
-if __name__ == '__main__':
+async def main():
     parser = argparse.ArgumentParser(
         prog = 'Discord RSS Socials',
         description= 'Polls RSS feeds and posts updates to Discord via a webhook.',
@@ -198,12 +191,31 @@ if __name__ == '__main__':
     parser.add_argument('-wh', '--webhook', action=EnvDefault, type=str, envvar='DISCORD_WEBHOOK',
                         help='A Discord webhook URL. You can generate this for a channel under Edit Channel/Integrations/Webhooks.\n' \
                             'Can also be specified using DISCORD_WEBHOOK environment variable.')
+    parser.add_argument('-l', '--logs', action=EnvDefault, type=str, envvar='LOG_FILE_PATH', required=False,
+                        help='Path to a log file to write to.\n' \
+                            'Can also be specified using LOG_FILE_PATH environment variable.')
     parser.add_argument('-s', '--since', type=str,
                         help='Specify a time to use as cutoff point for new posts (yyyy-mm-dd HH:MM). Always in UTC.')
     parser.add_argument('-r', '--render', action='store_true',
                         help="Displays application status in a nicely rendered terminal window.")
     
     args = parser.parse_args()
+
+    FORMAT = "%(message)s"
+    handlers = [RichHandler()]
+    if args.logs:
+        p = Path(args.logs)
+        if p.parent is not None and p.parent.is_dir:
+            p.parent.mkdir(parents=True, exist_ok=True)
+            
+        file_log = RotatingFileHandler(
+            filename=args.logs,
+            mode='a',
+            maxBytes=1e+7, # 10 mb
+            backupCount=3
+        )
+        handlers.append(file_log)
+    logging.basicConfig(level='NOTSET', format=FORMAT, datefmt="[%X]", handlers=handlers)
 
     since = datetime.datetime.now(datetime.UTC)
     if args.since is not None:
@@ -212,4 +224,44 @@ if __name__ == '__main__':
         except ValueError:
             raise argparse.ArgumentTypeError(f"not a valid date: {args.since!r}")
 
-    asyncio.run(main(args.filename, args.webhook, since, args.render))
+    filename = args.filename
+    webhook_url = args.webhook
+    should_render = args.render
+
+    feeds = []
+    async with aiofiles.open(filename, mode='r') as f:
+        line_no = 0
+        async for line in f:
+            line_no += 1
+            if not is_valid_url(line):
+                logger.warning(f"{filename}:{line_no}: \"{line}\" is not a valid url. Skipping.")
+                continue
+                
+            feeds.append(RssFeed(line))
+
+    if len(feeds) == 0:
+        logger.error(f"\"{filename}\" has no entries")
+        return
+
+    tasks = []
+    queue = asyncio.Queue[RssEntry]()
+
+    # Spin up poll routine which adds new posts to the queue
+    tasks.append(asyncio.create_task(check_feeds_task(feeds, since, queue)))
+
+    # Spin up queue routine which pushes out new webhooks
+    tasks.append(asyncio.create_task(webhook_queue_task(webhook_url, queue)))
+
+    # Spin up rendering routine if requested
+    if should_render == True:
+        tasks.append(asyncio.create_task(render(feeds, since)))
+
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Program interrupted by user")
+    except Exception:
+        logger.exception("Unhandled exception")
